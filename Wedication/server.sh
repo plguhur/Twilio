d8bc5176957dccba8e46d83045e774035246c07d
Pierre-Louis Guhur <pierre-louis.guhur@mv-robot.com>
2018-04-24 15:40:07 +0000

    fix issue

---
+++ Wedication/server.sh
@@ -0,0 +1,5 @@
+export Twilio_workbook="60ans_PP" 
+export Twilio_account_per="ACc0f384d057e028db7c50c8f5174de10a"
+export Twilio_account_token_per="ead4d26a2dabc514ea0a78d658cc105a"
+export SERVER_IP="46.101.115.37"
+python hello_guest.py  --noauth_local_webserver
--- Wedication/sheets.py
+++ Wedication/sheets.py
@@ -1,8 +1,9 @@
 import json
 import time
 import gspread
-from oauth2client.client import SignedJwtAssertionCredentials
 from twilio.rest  import Client
+import os
+from utils import *
 
 #Message your attendees from a spreadsheet
 ACCOUNT_SID = os.environ['Twilio_account_per']
@@ -12,7 +13,8 @@ WORKBOOK    = os.environ['Twilio_workbook']
 json_key = json.load(open('client_secret.json'))#add file name for the json created for the spread sheet
 scope = ['https://spreadsheets.google.com/feeds']
 
-credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+#credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+credentials = get_credentials(CLIENT_SECRET_FILE, SCOPES, APPLICATION_NAME)
 gc = gspread.authorize(credentials)
 wks = gc.open(WORKBOOK) #add your workbook name here
 wks_attendees = wks.get_worksheet(0) #attendees worksheet
@@ -21,22 +23,23 @@ client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 
 for num in range(2,60):  #to iterate between guests, amend this based on your total
-    print "sleeping for 2 seconds"
+    print("sleeping for 2 seconds")
     time.sleep(2) #adding a delay to avoid filtering
 
     guest_number = wks_attendees.acell('B' +str(num)).value
     guest_name = wks_attendees.acell('A'+str(num)).value
+    accepted   = wks_attendees.acell('F'+str(num)).value
 
     if not  guest_number:
-        print guest_name + ' telephone number empty not messaging'
+        print(guest_name + ' telephone number empty not messaging')
         wks_attendees.update_acell('E'+str(num), '0') #set number to 0
 
-    else:
-        print  'Sending message to ' + guest_name
+    elif len(accepted) == 0:
+        print('Envoi d\'un message à ' + guest_name)
         client.messages.create(
             to="+" + guest_number,
-            from_="", #your twilio number here
-            body= message_body = u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + "\n\n" + u"\u2709"
+            from_="33757915976",#+33757915976", #your twilio number here
+            body=  u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + u"\u2B50" + u"\u1F381" + "\n\n" + u"\u2709"
             +" Venez jouer les groupies et casser les fauteuils à l'occasion des 60 ans de Pascal Piroux ! "+  u"\u2709" +
             "\n\nLe samedi 1er septembre 2018 de *midi précise* à minuit.\n\n \
             Manoir Carrefour de l'Obélisque, 77174 Villeneuve le Comte. \n\n  \
@@ -46,4 +49,4 @@ for num in range(2,60):  #to iterate between guests, amend this based on your to
         )
         wks_attendees.update_acell('E'+str(num), int(wks_attendees.acell('E' +str(num)).value) + 1) #increment the message count row
 else:                  # else part of the loop
-   print 'finished'
+   print('finished')
